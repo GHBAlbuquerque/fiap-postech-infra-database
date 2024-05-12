89e21360bcdcdd1c728d12072668d06182cb6bfa
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-12 13:44:26 -0300

    ajuste nome do data

--- infra/main.tf
+++ infra/main.tf
@@ -53,7 +53,7 @@ resource "mongodbatlas_advanced_cluster" "atlas-cluster" {
   }
 }
 
-data "mongodbatlas_advanced_cluster" "atlas-cluser" {
+data "mongodbatlas_advanced_cluster" "atlas-cluster" {
   project_id = mongodbatlas_project.atlas-project.id
   name       = mongodbatlas_advanced_cluster.atlas-cluster.name
   depends_on = [mongodbatlas_privatelink_endpoint_service.atlaseplink]
